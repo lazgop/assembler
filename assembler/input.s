b10519ca72b2474ad40f13013ecd34cea1268063
Lazar Gopcevic <lazgop@gmail.com>
2017-05-29 19:16:08 +0200

    Change input program

--- assembler/input.s
+++ assembler/input.s
@@ -1,20 +1,30 @@
 .data
-n: DW 10000
+n: DD 5555
 ORG 0x100
 .text
 .global START
  START:
  LOAD R1, #timer ; get address of timer routine
  STORE R1, 4*4 ; store address of timer routine in IVT entry 4
- LOAD R1, #1
-loop:
- LOADSW R0, n
- SUB R0, R0, R1
- STOREW R0, n
+ LOAD R1, #keyboard ; get address of keyboard routine
+ STORE R1, 5*4 ; store address of keyboard routine in IVT entry 5
+ LOAD R1, #error ; get address of error routine
+ STORE R1, 3*4 ; store address of error routine in IVT entry 3
+ LOAD R1, #48 ; store ascii for '0' in R1
+loop: ; loop until '0' is entered on keyboard
+ SUB R0, R1, R5
+ DB 88
  JNZ R0, loop
  INT 0
 
+error:
+  INT 0
+
 timer:
   LOAD R4, #4
+  RET
+
+keyboard:
+  LOADUB R5, 33*4 ; read char from inputreg
   RET
 .end
